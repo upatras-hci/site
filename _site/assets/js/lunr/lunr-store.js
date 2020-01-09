3b51f84cac0576272083d1a973efc2298ca029ca
moya10 <56644086+moya10@users.noreply.github.com>
2020-01-09 02:07:19 +0200

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,116 @@
+var store = [{
+        "title": "Articifial Intelligence",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/courses/ai/",
+        "teaser":null},{
+        "title": "Collaboration Technologies",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/courses/cscw/",
+        "teaser":null},{
+        "title": "Design and Evaluation of Interactive Systems",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/courses/deis/",
+        "teaser":null},{
+        "title": "Front End Web Programming",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/courses/fep/",
+        "teaser":null},{
+        "title": "Introduction to Human- Robot Interaction",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/courses/hri/",
+        "teaser":null},{
+        "title": "Design of Location-based applications",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/courses/lba/",
+        "teaser":null},{
+        "title": "Software Quality",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/courses/sq/",
+        "teaser":null},{
+        "title": "Ubiquitous Computing",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/courses/ubiq/",
+        "teaser":null},{
+        "title": "Nikolaos Avouris",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/avouris/",
+        "teaser":null},{
+        "title": "Κωνσταντίνος Χωριανόπουλος",
+        "excerpt":"Education:  “Δίπλωμα Ηλεκτρονικών Μηχανικών και Μηχανικών Υπολογιστών, Πολυτεχνείο Κρήτης”  “Μεταπτυχιακό Μάρκετινγκ και Επικοινωνία με Νέες Τεχνολογίες, Τμήμα Μάρκετινγκ και Επικοινωνίας, Οικονομικό Πανεπιστήμιο Αθηνών”  “Διδακτορικό Δίπλωμα, Επικοινωνία Ανθρώπου-Υπολογιστή για την Ψηφιακή Τηλεόραση, Τμήμα Διοικητικής Επιστήμης και Τεχνολογίας, Οικονομικό Πανεπιστήμιο Αθηνών”","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/choko/",
+        "teaser":null},{
+        "title": "Evangelos Dermatas",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/dermatas/",
+        "teaser":null},{
+        "title": "Ioannis Hatzilygeroudis",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/ihatz/",
+        "teaser":null},{
+        "title": "Nikos Karacapilidis",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/karacap/",
+        "teaser":null},{
+        "title": "Nicolas Marmaras",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/marmaras/",
+        "teaser":null},{
+        "title": "Mavrommati Irene",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/mavrommati/",
+        "teaser":null},{
+        "title": "Konstantinos Moustakas",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/mouko/",
+        "teaser":null},{
+        "title": "Maria Rigou",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/rigou/",
+        "teaser":null},{
+        "title": "Sophia Daskalaki",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/sdask/",
+        "teaser":null},{
+        "title": "Kyriakos Sgarbas",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/sgarbas/",
+        "teaser":null},{
+        "title": "Michalis Xenos",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/people/xenos/",
+        "teaser":null},{
+        "title": "HCI Master Orientation Event",
+        "excerpt":"On October 3rd the Introductory and Orientation event of the HCI Master for academic year 2019-2020 took place. The event was opened by a welcome message of Prof. Stavros Koubias, Head of the Electrical and Computer Engineering Department and a welcome address by Prof. Stratis Gallopoulos, Head of the Computer...","categories": ["secretary","students"],
+        "tags": ["orientation event"],
+        "url": "http://localhost:4000/site/posts/2019/10/03/HCIMasterOrientationEvent/",
+        "teaser":null},{
+        "title": "Ieeevr",
+        "excerpt":"title: “IEEE VR Contest”excerpt: “Expression of interest for IEEE VR Contest”category: studentstags: IEEE VR Contest Αγαπητοί φοιτητές, Την ερχόμενη Παρασκευή 18/10, στις 11 το πρωί, στο χώρο που κάνουμε εργαστήριο, θα οριστούν οι ομάδες που θα συμεμτάσχουν στο διαγωνισμό του ΙΕΕΕ VR.Παρακαλώ όσες ομάδες φοιτητών, αλλά και μεμονωμένα άτομα, ενδιαφέρονται...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site/posts/2019/10/14/IEEEVR/",
+        "teaser":null},{
+        "title": "Call for applications 2020-2021",
+        "excerpt":"Expression of interest for admission of new students for the course starting in October 2020.Eligibility criteriaThe candidates should hold an undergraduate degree in the fields of Computer Engineering, Informatics, Electronic/Electrical Engineering, Physics, Mathematics or similar science fields, from recognised institutions in Greece or abroad. Applications may also be submitted by...","categories": ["secretary","students"],
+        "tags": ["admission","application"],
+        "url": "http://localhost:4000/site/posts/2019/11/23/call-for-2020-2021/",
+        "teaser":null}]
