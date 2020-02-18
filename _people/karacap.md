dcdc87538d2733d04c307cdb25438f8332b89e32
moya10 <ece7992@upnet.gr>
2020-02-18 22:19:56 +0100

    google_maps
    Προσθήκη χάρτη και σύνδεση με την εφαρμογή πλοήγησης στο κινητό του χρήστη

---
+++ _people/karacap.md
@@ -0,0 +1,11 @@
+---
+ref: karacap
+title: "Nikos Karacapilidis"
+excerpt: ""
+rank: 3
+author: karacap
+research: 
+---
+
+### Education:
+  
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,116 @@
+var store = [{
+        "title": "Articifial Intelligence",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/courses/ai/",
+        "teaser":null},{
+        "title": "Collaboration Technologies",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/courses/cscw/",
+        "teaser":null},{
+        "title": "Design and Evaluation of Interactive Systems",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/courses/deis/",
+        "teaser":null},{
+        "title": "Front End Web Programming",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/courses/fep/",
+        "teaser":null},{
+        "title": "Introduction to Human- Robot Interaction",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/courses/hri/",
+        "teaser":null},{
+        "title": "Design of Location-based applications",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/courses/lba/",
+        "teaser":null},{
+        "title": "Software Quality",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/courses/sq/",
+        "teaser":null},{
+        "title": "Ubiquitous Computing",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/courses/ubiq/",
+        "teaser":null},{
+        "title": "Nikolaos Avouris",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/avouris/",
+        "teaser":null},{
+        "title": "Konstantinos Chorianopoulos",
+        "excerpt":"Education:  “Δίπλωμα Ηλεκτρονικών Μηχανικών και Μηχανικών Υπολογιστών, Πολυτεχνείο Κρήτης”  “Μεταπτυχιακό Μάρκετινγκ και Επικοινωνία με Νέες Τεχνολογίες, Τμήμα Μάρκετινγκ και Επικοινωνίας, Οικονομικό Πανεπιστήμιο Αθηνών”  “Διδακτορικό Δίπλωμα, Επικοινωνία Ανθρώπου-Υπολογιστή για την Ψηφιακή Τηλεόραση, Τμήμα Διοικητικής Επιστήμης και Τεχνολογίας, Οικονομικό Πανεπιστήμιο Αθηνών”","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/choko/",
+        "teaser":null},{
+        "title": "Evangelos Dermatas",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/dermatas/",
+        "teaser":null},{
+        "title": "Ioannis Hatzilygeroudis",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/ihatz/",
+        "teaser":null},{
+        "title": "Nikos Karacapilidis",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/karacap/",
+        "teaser":null},{
+        "title": "Nicolas Marmaras",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/marmaras/",
+        "teaser":null},{
+        "title": "Mavrommati Irene",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/mavrommati/",
+        "teaser":null},{
+        "title": "Konstantinos Moustakas",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/mouko/",
+        "teaser":null},{
+        "title": "Maria Rigou",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/rigou/",
+        "teaser":null},{
+        "title": "Sophia Daskalaki",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/sdask/",
+        "teaser":null},{
+        "title": "Kyriakos Sgarbas",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/sgarbas/",
+        "teaser":null},{
+        "title": "Michalis Xenos",
+        "excerpt":"Education:","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/site-gr/people/xenos/",
+        "teaser":null},{
+        "title": "HCI Master Orientation Event",
+        "excerpt":"On October 3rd the Introductory and Orientation event of the HCI Master for academic year 2019-2020 took place. The event was opened by a welcome message of Prof. Stavros Koubias, Head of the Electrical and Computer Engineering Department and a welcome address by Prof. Stratis Gallopoulos, Head of the Computer...","categories": ["secretary","students"],
+        "tags": ["orientation event"],
+        "url": "http://localhost:4000/site-gr/posts/2019/10/03/HCIMasterOrientationEvent/",
+        "teaser":null},{
+        "title": "IEEE VR Contest",
+        "excerpt":"Αγαπητοί φοιτητές,Την ερχόμενη Παρασκευή 18/10, στις 11 το πρωί, στο χώρο που κάνουμε εργαστήριο, θα οριστούν οι ομάδες που θα συμεμτάσχουν στο διαγωνισμό του ΙΕΕΕ VR.Παρακαλώ όσες ομάδες φοιτητών, αλλά και μεμονωμένα άτομα, ενδιαφέρονται να έρθουν για συζήτησημαζί με θεματικές προτάσεις επί του προβλήματος του διαγωνισμού για να οριστικοποιήσουμε τις...","categories": ["students"],
+        "tags": ["IEEE VR","Contest"],
+        "url": "http://localhost:4000/site-gr/posts/2019/10/14/IEEEVR/",
+        "teaser":null},{
+        "title": "Call for applications 2020-2021",
+        "excerpt":"Expression of interest for admission of new students for the course starting in October 2020.Eligibility criteriaThe candidates should hold an undergraduate degree in the fields of Computer Engineering, Informatics, Electronic/Electrical Engineering, Physics, Mathematics or similar science fields, from recognised institutions in Greece or abroad. Applications may also be submitted by...","categories": ["secretary","students"],
+        "tags": ["admission","application"],
+        "url": "http://localhost:4000/site-gr/posts/2019/11/23/call-for-2020-2021/",
+        "teaser":null}]
